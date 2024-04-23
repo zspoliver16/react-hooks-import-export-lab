7eb0446d1aeafdc50594f3f1ace5a0c96cafb8b2
zachary oliver <zspoliver@gmail.com>
2024-04-23 13:31:53 -0400

    adds index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import About from "./About.js";
+import Home from "./Home.js";
+import NavBar from "./NavBar.js";
 
 function App() {
   return (
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {username, city} from "../data/user.js"
 
 function Home() {
   return (
@@ -9,3 +10,5 @@ function Home() {
     </div>
   );
 }
+
+export default Home
